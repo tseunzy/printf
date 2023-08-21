3dac2da6e9dfba0feb16aa0d995a1fb92b738759
Bra Koo$e <72326008+BraKoose@users.noreply.github.com>
2023-08-21 20:20:02 +0000

    Update _printf.c
    Switch Statement

--- _printf.c
+++ _printf.c
@@ -19,33 +19,33 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
 
-	print_buffer(buffer, &buff_ind);
+for (i = 0; format && format[i] != '\0'; i++) {
+    switch (format[i]) {
+        case '%':
+            print_buffer(buffer, &buff_ind);
+            flags = get_flags(format, &i);
+            width = get_width(format, &i, list);
+            precision = get_precision(format, &i, list);
+            size = get_size(format, &i);
+            ++i;
+            printed = handle_print(format, &i, list, buffer,
+                flags, width, precision, size);
+            if (printed == -1)
+                return (-1);
+            printed_chars += printed;
+            break;
+        default:
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+            /* write(1, &format[i], 1);*/
+            printed_chars++;
+            break;
+    }
+} 
+
+        print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
