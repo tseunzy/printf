1f1b27625be7d7be78f2cee30b3d45def26e13ec
root <Godfred Addai Amoako>
2023-08-21 14:22:50 -0700

    buffa alx

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int k, pfrinted = 0, fprinted_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -19,52 +19,49 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
+	for (k = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			pfrinted = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (fprinted == -1)
+				return (-1);
+			fprinted_chars += pfrinted;
+		}
+	}
 
-    // Loop through the format string
-    for (i = 0; format && format[i] != '\0'; i++) {
-        switch (format[i]) {
-            case '%':
-                print_buffer(buffer, &buff_ind);
-                flags = get_flags(format, &i);
-                width = get_width(format, &i, list);
-                precision = get_precision(format, &i, list);
-                size = get_size(format, &i);
-                ++i;
-                printed = handle_print(format, &i, list, buffer,
-                    flags, width, precision, size);
-                if (printed == -1)
-                    return (-1);
-                printed_chars += printed;
-                break;
-            default:
-                buffer[buff_ind++] = format[i];
-                if (buff_ind == BUFF_SIZE)
-                    print_buffer(buffer, &buff_ind);
-                /* write(1, &format[i], 1); */
-                printed_chars++;
-                break;
-        }
-    }
-
-    // Print any remaining buffered content
-
-        print_buffer(buffer, &buff_ind);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
-	return (printed_chars);
+	return (fprinted_chars);
 }
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buffa: Array of chars
+ * @buff_inid: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_buffer(char buffa[], int *buff_inid)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+	if (*buff_inid > 0)
+		write(1, &buffa[0], *buff_inid);
 
-	*buff_ind = 0;
+	*buff_inid = 0;
 }
 
