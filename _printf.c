ff766db11a1e0cde0ca64eb527b49687cc476fc5
root <Godfred Addai Amoako>
2023-08-21 14:37:39 -0700

    same result

--- _printf.c
+++ _printf.c
@@ -10,42 +10,42 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int k, pfrinted = 0, fprinted_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	int flags, width, precision, size, buff_inid = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
+	char buffa[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
 
-	for (k = 0; format && format[i] != '\0'; i++)
+	for (k = 0; format && format[k] != '\0'; k++)
 	{
-		if (format[i] != '%')
+		if (format[k] != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			buffa[buff_inid++] = format[k];
+			if (buff_inid == BUFF_SIZE)
+				print_buffer(buffa, &buff_inid);
+			/* write(1, &format[k], 1);*/
+			fprinted_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			pfrinted = handle_print(format, &i, list, buffer,
+			print_buffer(buffa, &buff_inid);
+			flags = get_flags(format, &k);
+			width = get_width(format, &k, list);
+			precision = get_precision(format, &k, list);
+			size = get_size(format, &k);
+			++k;
+			pfrinted = handle_print(format, &k, list, buffa,
 				flags, width, precision, size);
-			if (fprinted == -1)
+			if (pfrinted == -1)
 				return (-1);
 			fprinted_chars += pfrinted;
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_buffer(buffa, &buff_inid);
 
 	va_end(list);
 
